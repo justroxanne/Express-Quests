4fd2adfc4ea46953d612547e6496eb7d5c43241e
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-05-08 17:49:37 +0200

    quest 05 delete method done

--- movieHandlers.js
+++ movieHandlers.js
@@ -16,7 +16,7 @@ const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query('select * from movies where id = ?', [id])
+    .query('SELECT * FROM movies WHERE id = ?', [id])
     .then(([movies]) => {
       if (movies[0] != null) {
         res.json(movies[0]);
@@ -51,11 +51,10 @@ const postMovie = (req, res) => {
 
 const updateMovie = (req, res) => {
   const id = parseInt(req.params.id);
-  const { title, director, year, color, duration } = req.body;
 
   database
     .query(
-      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      "UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
       [title, director, year, color, duration, id]
     )
     .then(([result]) => {
@@ -71,6 +70,27 @@ const updateMovie = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      "DELETE FROM movies WHERE id = ?",
+      [id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.status(204).send("No Content");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+}
+
 
 
 module.exports = {
@@ -78,4 +98,5 @@ module.exports = {
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie
 };
--- userHandlers.js
+++ userHandlers.js
@@ -16,7 +16,7 @@ const getUserById = (req, res) => {
 const id = parseInt(req.params.id);
 
     database
-        .query('select * from users where id = ?', [id])
+        .query('SELECT * FROM users WHERE id = ?', [id])
         .then(([users]) => {
             if (users[0] != null) {
                 res.json(users[0]);
@@ -63,13 +63,36 @@ const updateUser = (req, res) => {
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send('Error editing the user');
+            res.status(500).send("Error editing the user");
         });
 }
+
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query(
+            "DELETE FROM users WHERE id = ?",
+            [id]
+        )
+        .then(([result]) => {
+            if(result.affectedRows === 0) {
+                res.status(404).send('Not Found');
+            } else {
+                res.status(204).send('No Content');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error deleting the user');
+        });
+}
+
 
 module.exports = {
     getUsers,
     getUserById,
     addUser,
-    updateUser
+    updateUser,
+    deleteUser
 };
